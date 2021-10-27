74772db507a9c081dc02b7c4183005e32654dadc
mateusz-stasinski <m.stasinski12@gmail.com>
2021-10-27 14:14:48 +0200

    Fefactory for multiple modules

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { IProduct } from './product';
-import { ProductListComponent } from './product-list.component';
+import { ProductService } from './product.service';
 
 @Component({
   selector: 'pm-product-detail',
@@ -14,19 +14,19 @@ export class ProductDetailComponent implements OnInit {
 
   constructor(private route: ActivatedRoute,
               private router: Router,
-              private productListComponent: ProductListComponent) { }
+              private productService: ProductService) { }
 
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
     this.pageTitle += `: ${id}`;
-    this.product = this.productListComponent.filteredProducts.find((pr: IProduct) => pr.productId == id);
-    console.log(this.product);
+    this.productService.getProducts().subscribe(data => {
+      this.product = data.find(d => d.productId === id);
+      console.log(this.product);
+    })
   }
 
   onBack(): void {
     this.router.navigate(['/products']);
   }
-
   
-
-}
+ }
